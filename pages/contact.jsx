1b458bf18c658d20ff336926a922d8e71f3076ca
kaushik <kaushikrajput0926@gmail.com>
2023-05-03 20:52:51 +0530

    initial commit

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -90,11 +90,11 @@ const Contact = (props) => {
                                 </Link>
                             </div>
                         </div>
-                        <div className='col-md-6'>
+                        {/* <div className='col-md-6'>
                             <div className='google-map'>
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4603.194813717168!2d77.34558617859463!3d28.633687107097007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1681545614815!5m2!1sen!2sin" width="700" height="450" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
